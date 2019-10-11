d74ff4a55a7ff8e97a0f0cf67d893c44bc03257b
duwnwj <duanwj6563@gmail.com>
2019-10-11 22:20:01 +0800

    全局异常处理

---
+++ src/main/java/com/duanwj/learn/ExceptionTest.java
@@ -0,0 +1,16 @@
+package com.duanwj.learn;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 15737
+ */
+@Slf4j
+public class ExceptionTest {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 0;
+        int c = a / b;
+        //todo 全局异常处理
+    }
+}
